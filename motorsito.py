17ed80a7f8c296be3346257b2167a820c80671d0
davidalejandro04 <da.pabon10@uniandes.edu.co>
2017-11-30 22:19:22 -0500

    Update motorsito.py
    Update

--- motorsito.py
+++ motorsito.py
@@ -11,24 +11,25 @@ Motor2B = 37
 Motor1A = 29
 Motor1B = 31
 
-v1=100
-v2=40
-
 GPIO.setup(Motor1A,GPIO.OUT)
 GPIO.setup(Motor1B,GPIO.OUT)
 GPIO.setup(Motor2A,GPIO.OUT)
 GPIO.setup(Motor2B,GPIO.OUT)
 
-pwm=GPIO.PWM(29,v1)
-pwm2=GPIO.PWM(35,v2)
+pwm=GPIO.PWM(29,40)
+pwm2=GPIO.PWM(35,100)
 
-
+pwm3=GPIO.PWM(35,40)
+pwm4=GPIO.PWM(37,100)
 
 pwm.start(100)
 pwm2.start(100)
 
+pwm3.start(100)
+pwm4.start(100)
 
 def Forward(v1,v2):
+  
   pwm.ChangeFrequency(v1*.5)
   pwm2.ChangeFrequency(v2*.5)
 
@@ -44,10 +45,10 @@ def Forward(v1,v2):
   return 
 
 
-def Forward(v1,v2):
+def Backward(v1,v2):
 
-  pwm.ChangeDutyCycle(v1*.5)
-  pwm2.ChangeDutyCycle(v2*.5)
+  pwm3.ChangeFrequency(v1*.5)
+  pwm4.ChangeFrequency(v2*.5)
 
   GPIO.output(Motor1A,GPIO.LOW)
   GPIO.output(Motor1B,GPIO.HIGH)
@@ -61,6 +62,11 @@ def Forward(v1,v2):
 
 pwm.stop()
 pwm2.stop()
+pwm3.stop()
+pwm4.stop()
+
+Forward(100,100)
+Backward(100,100)
 
 GPIO.cleanup()
 
