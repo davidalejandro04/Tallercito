299c8f9a0c37f2ca3c5bacbca2933add7fe7bf68
davidalejandro04 <da.pabon10@uniandes.edu.co>
2017-12-01 04:36:41 -0500

    Update motorsito.py
    ASdf

--- motorsito.py
+++ motorsito.py
@@ -19,14 +19,8 @@ GPIO.setup(Motor2B,GPIO.OUT)
 pwm=GPIO.PWM(29,40)
 pwm2=GPIO.PWM(35,100)
 
-pwm3=GPIO.PWM(35,40)
-pwm4=GPIO.PWM(37,100)
-
 pwm.start(100)
 pwm2.start(100)
-
-pwm3.start(100)
-pwm4.start(100)
 
 def Forward(v1,v2):
   
@@ -44,30 +38,11 @@ def Forward(v1,v2):
  
   return 
 
-
-def Backward(v1,v2):
-
-  pwm3.ChangeFrequency(v1*.5)
-  pwm4.ChangeFrequency(v2*.5)
-
-  GPIO.output(Motor1A,GPIO.LOW)
-  GPIO.output(Motor1B,GPIO.HIGH)
-
-  GPIO.output(Motor2A,GPIO.LOW)
-  GPIO.output(Motor2B,GPIO.HIGH)
-
-  sleep(2)
-
-  return 
+Forward(100,100)
+Forward(100,10)
+Forward(10,100)
 
 pwm.stop()
 pwm2.stop()
-pwm3.stop()
-pwm4.stop()
-
-Forward(100,100)
-Backward(100,100)
 
 GPIO.cleanup()
-
-
